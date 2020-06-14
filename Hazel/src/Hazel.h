78879f5613c7094494e6ba8c2d439194d851d304
Damian Black <damianblack.wolf@gmail.com>
2020-06-14 17:03:51 -0400

    Set up basic application and entry point

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// for use by Hazel applications
+
+#include "Hazel/Application.h"
+
+// entry point
+#include "Hazel/EntryPoint.h"
+// ~entry point
---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "core.h"
+
+namespace Hazel {
+
+	class HAZEL_API Application
+	{
+	public:
+		Application();
+		virtual ~Application();
+
+		void Run();
+	};
+
+	// to be defined in a client
+	Application* CreateApplication();
+
+}
